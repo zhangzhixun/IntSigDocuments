28b0e46123b66e1a1dedf4d913c09c9e2a4264aa
zhixun zhang <452758079@qq.com>
2013-11-25 14:39:59 +0800

    20131125
    MRC demo

---
+++ MRCDemo/MRCDemo/NTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NTAppDelegate.h
+//  MRCDemo
+//
+//  Created by 张志勋 on 13-11-25.
+//  Copyright (c) 2013年 IntSig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
