6d5f6300bfbafb4da515e278a608c58cb3323c80
张志勋 <zhixun_zhang@IntSigdeiMac-2.local>
2014-04-01 17:07:07 +0800

    图片缩放裁剪功能

---
+++ CropImageDemo/CropImageDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CropImageDemo
+//
+//  Created by 张志勋 on 14-4-1.
+//  Copyright (c) 2014年 zhixun_zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
